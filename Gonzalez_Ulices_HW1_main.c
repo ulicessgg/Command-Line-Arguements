65b10db1d3771f65aa9833d5c07ce91131835901
Ulices Gonzalez <sgonzalez30@sfsu.edu>
2024-08-30 18:53:32 -0700

    added header to main file and starting work on assignment 1

--- Gonzalez_Ulices_HW1_main.c
+++ Gonzalez_Ulices_HW1_main.c
@@ -1,3 +1,18 @@
+/**************************************************************
+* Class::  CSC-415-01 Fall 2024
+* Name:: Ulices Gonzalez
+* Student ID:: 923328897
+* GitHub-Name:: ulicessgg
+* Project:: Assignment 1 – Command Line Arguments
+*
+* File:: Gonzalez_Ulices_HW1_main.c
+*
+* Description:: This file handles the fetching of command line
+* arguements and displays them to the user with their correct
+* index value based off their position in the arguement string
+*
+**************************************************************/
+
 #include <stdio.h>
 
 int main(void)
